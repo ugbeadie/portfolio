707b302e8227cc78a1a63c7065c66751a4b67165
ugbeadie <ugbeadie3@gmail.com>
2022-04-22 12:17:07 +0100

    fixed navbar bug

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const burger = document.getElementsByClassName('burger')[0];
+const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const navUl = document.getElementsByClassName('nav-items')[0];
+const navMenu = document.querySelector('nav')
+const close = document.querySelector('close')
 
-burger.addEventListener('click', () => {
+toggleButton.addEventListener('click', () => {
     navUl.classList.toggle('active');
+    navMenu.classList.toggle('active')
     document.body.classList.toggle('hidden')
-}) 
+    document.body.classList.toggle("black");
+    document.body.classList.toggle("cross");
+})
 
 // window.addEventListener ('scroll', function() {
 //     let navBar = document.querySelector('nav');
@@ -12,8 +17,9 @@ burger.addEventListener('click', () => {
 //     navBar.classList.toggle('scrolling-active', windowPosition);
 // })
 
-// function hideUl() {
-//     document.body.classList.toggle('close');
-//     document.body.classList.toggle('hidden');
-//     navUl.classList.toggle('active');
-// }
+function hideUl() {
+    document.body.classList.toggle("cross");
+    document.body.classList.toggle('hidden');
+    navUl.classList.toggle('active');
+    document.body.classList.toggle("black");    
+}
