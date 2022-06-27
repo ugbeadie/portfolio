e6c4a3448134d1fbdc754a026c915499e40c3f5f
ugbeadie <ugbeadie3@gmail.com>
2022-06-27 11:05:48 +0100

    fix navbar bug and add new feature

--- script.js
+++ script.js
@@ -2,34 +2,35 @@ const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const navUl = document.getElementsByClassName('nav-items')[0];
 const navMenu = document.querySelector('nav')
 const close = document.querySelector('close')
+const topLink = document.querySelector('.top-link')
 
 toggleButton.addEventListener('click', () => {
     navUl.classList.toggle('active');
-    navMenu.classList.toggle('active')
+    // navMenu.classList.toggle('black')
     document.body.classList.toggle('hidden')
+    document.body.classList.toggle('black')
     document.body.classList.toggle("cross");
 })
 
 window.addEventListener('scroll',  function() {
-    if (window.scrollY > 500) {
+    if (window.scrollY > 600) {
         navMenu.style.position = 'fixed';
         navMenu.style.backgroundColor = 'rgb(12, 13, 13)';
+        topLink.classList.add("show-link");
+
     }else{
         navMenu.style.position = 'absolute';
         navMenu.style.backgroundColor = "var(--nav-bg)";
+        topLink.classList.remove("show-link");
+
     }
 });
-
-
-// window.addEventListener ('scroll', function() {
-//     let navBar = document.querySelector('nav');
-//     let windowPosition =  window.scrollY > 600;
-//     navBar.classList.toggle('scrolling-active', windowPosition);
-// })
 
 function hideUl() {
     document.body.classList.toggle("cross");
     document.body.classList.toggle('hidden');
     navUl.classList.toggle('active');
+    // navMenu.classList.toggle('black');
+    // navMenu.classList.remove('active')
     document.body.classList.toggle("black");    
 }
