e92dcf7cddd3c93d889b0cf67bb17003220b9740
ugbeadie <ugbeadie3@gmail.com>
2022-07-13 11:49:11 +0100

    remove swiper js nav

--- script.js
+++ script.js
@@ -39,11 +39,6 @@ const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
 const swiper = new Swiper('.swiper', {
-    // loop: true,
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
